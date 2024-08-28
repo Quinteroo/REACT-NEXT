25224d6f9da35a02c8ef08860c90d8a95613fbcb
quintero git <hello.quinteroo@gmail.com>
2024-08-28 19:08:43 +0200

    fear: nueva organización carpetas

---
+++ 2 Basic Hooks/2.0 IndiceContenido.jsx
@@ -0,0 +1,15 @@
+
+
+
+//! BASIC HOOKS
+// 00.00 introducción
+//* 01.30 VDOM - diferencia js vanilla y react
+// 10.00 crea y limpia proyecto react
+// 11.30 com ofunciona react con componentes
+// 16.00 crea eventos y funciones en componentes
+//* 19.00 cambia estilos en función de variables
+// 26.00 como funcionan los estados useState
+// 28.00 ¿por qué con variables no se renderizan lso cambios?
+// 30.. construye un estado ejemplo
+
+
