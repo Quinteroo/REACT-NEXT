979d2776ff117cd2e832188d9704ef80ac654fda
quintero git <hello.quinteroo@gmail.com>
2024-08-30 19:54:01 +0200

    mmmmm

--- 2 Basic Hooks/02-IndiceContenido.jsx
+++ 2 Basic Hooks/02-IndiceContenido.jsx
@@ -5,7 +5,7 @@
 // 00.00 introducción
 //* 01.30 VDOM - diferencia js vanilla y react
 // 10.00 crea y limpia proyecto react
-// 11.30 com ofunciona react con componentes
+// 11.30 como funciona react con componentes
 // 16.00 crea eventos y funciones en componentes
 //* 19.00 cambia estilos en función de variables
 // 26.00 como funcionan los estados useState
