6b798ed5653175ab24afeaa5c433dae89efbb83d
quintero git <hello.quinteroo@gmail.com>
2024-09-04 19:30:19 +0200

    inicio react router

---
+++ RTC/3 React Router/03.1-introducciónReactRouter.jsx
@@ -0,0 +1,61 @@
+//! ¿Qué es React Router DOM?
+// React Router DOM es una librería de enrutamiento diseñada específicamente para aplicaciones web desarrolladas con React.
+// Su objetivo principal es permitir la navegación entre diferentes componentes. Esto significa que,
+// React Router DOM actualiza dinámicamente la interfaz de usuario mediante la manipulación del DOM,
+// proporcionando una experiencia de navegación fluida y rápida.
+
+// Para instalar react-router-dom deberemos ejecutar el siguiente comando en la terminal en la ubicación de la raiz
+// de nuestro proyecto
+
+//* npm i react-router-dom
+
+
+
+//! React router Concepts
+//* Browser routes
+// accede a las rutas de nueatro navegador
+
+//* Routes
+// to dos los elementos de navegación que podemos emplear en nuestra aplicación web
+
+//* Route
+// es una ruta eterminada en la que indicamos que compoennte se tiene que renderizar
+
+//* link
+// permite navegar de un punto de app a otro
+
+//* Navlink
+// la diferencia es que navlink eniende el estado del componente, por ejemplo que ya te encuentras en "home"
+
+//* Outlet
+// lo que usamos dentro del componente como padre
+// permite renderizar todas las routes de los hijos del padre del que queramos
+
+//* Rutas Dinámicas
+// genera rutas en función de las respuestas del backend
+// por eejemplo de productos
+
+
+
+//! Puntos clave
+//* 1️⃣ Enrutamiento Declarativo:
+// React Router DOM utiliza un enfoque declarativo para definir las rutas de la aplicación,
+// lo que significa que se definen las relaciones entre las URL y los componentes de React de manera clara y estructurada.
+
+//* 2️⃣ Componentes Esenciales:
+// Ofrece componentes esenciales como <BrowserRouter>, <Route>, <Switch>, <Link>, <Outlet>,
+//   entre otros, que permiten configurar y gestionar las rutas de la aplicación.
+
+//* 3️⃣ Manejo de Rutas:
+// Permite la gestión eficiente de las URL de la aplicación, facilitando la navegación entre diferentes
+// vistas o páginas sin necesidad de recargar la página completa.
+
+//* 4️⃣ Parámetros de URL y Rutas Anidadas:
+// Permite la captura de parámetros de URL dinámicos y la anidación de rutas
+// para la construcción de interfaces complejas y estructuradas.
+
+//* 5️⃣ Integración con React:
+// React Router DOM se integra perfectamente con la biblioteca principal de React, lo que
+// significa que se utiliza de manera natural junto con otros componentes de React para
+// gestionar la navegación en la aplicación.
+
