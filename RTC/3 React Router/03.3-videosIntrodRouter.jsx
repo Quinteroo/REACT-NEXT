e4a8b63dcaee34e238e87a7c6d5431755306c0a6
quintero git <hello.quinteroo@gmail.com>
2024-09-05 20:12:55 +0200

    apuntes de componentes React Router

---
+++ RTC/3 React Router/03.3-videosIntrodRouter.jsx
@@ -0,0 +1,36 @@
+
+
+//! PARTE 1
+// 00.00 crea proyecto Vite-React
+//* 01.00 instala react router indicando versión
+//* 02.00 Rafce - crea componentes baásicos
+// 03.00 importa lso componentes de react router para emplear
+// 04.00 BrosetTrouter y basename
+// 06.00 index como elemento indexador
+// 08.00 ruta por parámetro
+// 08.20 ruta not found
+//* 09.30 termina el enrutado
+// 10.00 empieza a construir in header
+// 10.00 importa navlink y outlet
+// 15.00 termina el SPA
+
+
+
+//! PARTE 2
+// 00.00 empieza a desarrollar un poco la home
+//* 01.00 introduce link en componente home
+// 03.00 empieza a desarrollar compoennte Heroes
+// 06.00 termina de hacer data.js como si fuese la respuesta de una api
+// 06.15 empieza a escribir las funciones que simulan a peticiones
+// 08.42 termina de hacer las peticionesGet/POST/DELETE
+// 08.42 crea componente heroeDetail
+// 10.00 pinta los héroes con .map()
+// 12.00 arreglando el problema de recibir los datos de la api
+// 13.00 termina de pintar los héroes
+//* 13.30 emplea el componente de Router link
+// 15 crea componente de ficja del heroe
+//* useNavegate y useParams
+// 18.00 delete heroe
+//
+//
+//
