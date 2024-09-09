29e7f387b8157fc2d94ab609edcb73572a308e79
quintero git <hello.quinteroo@gmail.com>
2024-09-09 19:46:43 +0200

    apuntes TimingContenido Proyecto React Basic

--- RTC/4 Proyecto React Basic/04.1-TimingContenidoClaseDirecto.jsx
+++ RTC/4 Proyecto React Basic/04.1-TimingContenidoClaseDirecto.jsx
@@ -6,7 +6,16 @@
 // 05.00 crea proyecto Vite-React
 // 06.00 instalación liberías
 // 06.30 limpia proyecto
-//
+// 07.30 busca icono y coloca en metahead
+// 11.00 Color Magic - generador paleta colores según concepto
+// 11.40 index.css - estilos generales
+// 15.00 background animado
+// 16.00 componente - background animado
+// 19.27 Cntrl + H para selecionar todos los elementos iguales marcados
+// 21.00 BrowserRouter y router
+// 25.00 empieza a construir con useEffect el componente Characters
+// 30.00 crea el loading
+
 
 
 
