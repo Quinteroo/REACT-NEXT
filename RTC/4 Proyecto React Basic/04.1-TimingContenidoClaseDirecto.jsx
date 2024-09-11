c739c69c9288d2727d422ad0f8cd8c8deec46c58
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-09-11 16:52:48 +0200

    Update 04.1-TimingContenidoClaseDirecto.jsx

--- RTC/4 Proyecto React Basic/04.1-TimingContenidoClaseDirecto.jsx
+++ RTC/4 Proyecto React Basic/04.1-TimingContenidoClaseDirecto.jsx
@@ -13,7 +13,9 @@
 // 16.00 componente - background animado
 //* 19.27 Cntrl + H para selecionar todos los elementos iguales marcados
 // 21.00 BrowserRouter y router
-// 25.00 empieza a construir con useEffect el componente Characters
+// 23.00 crea componentes Characters y Character para ponerlos en el BrowserRouter
+// 25.00 Revisa la url de la api para traerse 20 personaes
+// empieza a construir con useEffect el componente Characters
 // 30.00 crea el useState loading y lo coloca dentro al abrir y cerrar el useEffect
 //* 31.00 pinta Characters en el main (siempre hay que indicar el jey (id) del objeto para que react no se pierda
 //* 33.00 - 35.00 crea el loading en el main con un gif
