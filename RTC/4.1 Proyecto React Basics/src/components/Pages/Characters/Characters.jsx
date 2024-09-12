d19b68dac5a4cd1defbf9c0eaa9d697cb79b2e03
quintero git <hello.quinteroo@gmail.com>
2024-09-12 20:00:47 +0200

    mejorar html charcaters.map y maquetación CSS

--- RTC/4.1 Proyecto React Basics/src/components/Pages/Characters/Characters.jsx
+++ RTC/4.1 Proyecto React Basics/src/components/Pages/Characters/Characters.jsx
@@ -10,30 +10,44 @@ import { useState, useEffect } from "react"
 const Characters = () => {
 
   const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(false)
+
 
   useEffect(() => {
+    setLoading(true)
+
     fetch("https://starwars-databank-server.vercel.app/api/v1/characters?page=1&limit=20")
       .then((res) => res.json())
       .then((res) => { setCharacters(res.data) });
+    setLoading(false)
   }, [])
 
 
 
   return (
-    <div className="image-character-container">
-      {characters.map((e) => (
-        <img className="image-character"
-          key={e._id}
-          src={e.image}
-        />
-      ))}
-    </div>
+    < section className="image-character-container" >
+      {
+        characters.map((e) => (
+          <div className="character-card" key={e._id}>
+            <div className="div-character-name">
+              <h2>{e.name}</h2>
+            </div>
+            <div className="img-wrp">
+              <img
+                src={e.image}
+                alt={e.name}
+              />
+            </div>
+          </div>
 
+        ))
+      }
 
-
-  )
-
+    </section >
+  );
 }
+
+
 
 
 
