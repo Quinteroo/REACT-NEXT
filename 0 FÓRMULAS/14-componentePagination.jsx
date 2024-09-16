356aca0a7c381f9f3d5f8c66fd8740277ce54ee5
quintero git <hello.quinteroo@gmail.com>
2024-09-16 18:57:46 +0200

    extraer pagination component y limit page

--- 0 FÓRMULAS/14-componentePagination.jsx
+++ 0 FÓRMULAS/14-componentePagination.jsx
@@ -9,7 +9,7 @@ const componentPagination = ({ page, setPage }) => {
   //! es 49 porque es el límite que hemos calculado de dividir el total de personajes que nos da la API(total: 956) por el 
   //! número que le hemos pedido que nos de por cada llamada (limit: 20)
   //! también podemos calcular limit en función de la elección del usuario.
-
+  //* en el proyecto presentado se puede ver el cáculo de limit
 }
 
 export default componentPagination
--- RTC/Proyecto React Basics/src/components/Pages/Characters/Characters.jsx
+++ RTC/Proyecto React Basics/src/components/Pages/Characters/Characters.jsx
@@ -4,6 +4,7 @@ import "./Characters.css"
 import { useState, useEffect } from "react"
 import Loading from "../../Loading/Loading.jsx"
 import CharacterCard from "../../CharacterCard/CharacterCard.jsx"
+import Pagination from "../../Pagination/Pagination.jsx"
 
 
 
@@ -14,6 +15,7 @@ const Characters = () => {
   const [characters, setCharacters] = useState([])
   const [loading, setLoading] = useState(false)
   const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(1)
 
 
   useEffect(() => {
@@ -22,8 +24,11 @@ const Characters = () => {
 
     fetch(`https://starwars-databank-server.vercel.app/api/v1/characters?page=${page}&limit=20`)
       .then((res) => res.json())
-      .then((res) => { setCharacters(res.data) });
-    setLoading(false)
+      .then((res) => {
+        setCharacters(res.data);
+        setLoading(false)
+        setLimit(Math.floor(res.info.total / res.info.limit) + 1)
+      });
   }, [page])
 
 
@@ -38,11 +43,7 @@ const Characters = () => {
         ))
       }
 
-      <div className="pagination">
-        <button onClick={() => setPage(page - 1)}>{"<"}</button>
-        <p>{page}</p>
-        <button onClick={() => setPage(page + 1)}>{">"}</button>
-      </div>
+      <Pagination page={page} setPage={setPage} limit={limit} />
 
     </section >
   );
