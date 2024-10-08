ed922dea4563e898f10cd78a3909848233632150
quintero git <hello.quinteroo@gmail.com>
2024-10-08 19:07:02 +0200

    wololooo

---
+++ RTC/5 React Avanzado/5.10 TimingCustomsHooks.jsx
@@ -0,0 +1,71 @@
+
+
+
+
+
+//! TIMING CONTENIDO
+// 01.00 crea proyecto para repasar useReducer
+// 03.20 explica useReducer
+// 04.55 crea useEffect en componente para petición API
+// 06.00 crea estados varios para probar antes de useREducer
+// 11.00 termina componente incluso con pagination
+// 12.00 error catch, busca solución
+// try catch
+//* 17.00 extrae funcionalidad componente
+// 18.00 peticion.action.js y peticion.reducer.js
+//* 19.00 construye reducer
+// 22.40 extrae petición fetch y lo mete en actions.js
+//* 25.00 disptach payload
+// 27.00 incorpora el useReducer en elc omponente donde se ha extgraido
+// elcódigo
+//* 29.00 modifica pagination con dispatch
+//* 30.00 explicación de por qué es más escalable
+//  explicación de como funciona useReducer y dispatch
+
+//? useMemo 45.00 =========================================================
+// 50.00 explicación useMemo
+// 55.00 crea array de objetos para pintar
+// 58.00 mwete array en useState
+//* 59.30 método toSorted()
+//* 01.01.00 useMemo
+
+//? useCallback 01.08.00 ===================================================
+// 01.20.00
+
+//? useRef 01.22.00==========
+// 01.29.00 sacar valor del input
+
+//? custom Hooks 01.38.00
+//* 01.40.00 componente modal
+// 01.42.00 como se crea custom Hooks
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
