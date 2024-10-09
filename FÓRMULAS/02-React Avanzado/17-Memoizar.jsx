7297ac8fd4565aaf9407d1210b00a858ce479206
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-10-09 12:58:51 +0200

    Update 17-Memoizar.jsx

--- FÓRMULAS/02-React Avanzado/17-Memoizar.jsx
+++ FÓRMULAS/02-React Avanzado/17-Memoizar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 //! el abuso de uso de memos, puede que haya un abuso de comprobaciones para no re-renderizar y relentizamos nuestra app
 
-const Movies = React.memo(({ title, poster }) => { //! tenemos que envolver TODO el componente para que no se renderice otroa vez
+const componenteDos = React.memo(({ title, poster }) => { //! tenemos que envolver TODO el componente para que no se renderice otroa vez
 
   console.log("renderizando Movies");
 
