332d007448875cd55682c42dd48bf4717f8e7bb3
qugodev <146204443+Quinteroo@users.noreply.github.com>
2024-10-17 15:55:14 +0200

    Update Form.jsx

--- FÓRMULAS/03-React Hook Form/Form.jsx
+++ FÓRMULAS/03-React Hook Form/Form.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form"
 
 const Form = () => {
 
-  const { handlesubmit, register, formState: { errors } } = useForm({
+  const { handleSubmit, register, formState: { errors } } = useForm({
     defaultValues: {
       userName: "",
       email: "",
@@ -17,19 +17,19 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={handlesubmit(submit)}>
+    <form onSubmit={handleSubmit(submit)}>
       <div>
         <label htmlFor="username">
           Nombre Usuario*
         </label>
         <input type="text" id="username"
-          {...register(userName),
-          {
-            required: {
-              value: true,
-              message: "necesitas poner un nombre ❌"
-            }
-          }}
+          {...register(userName,
+            {
+              required: {
+                value: true,
+                message: "necesitas poner un nombre ❌"
+              }
+            })}
         />
         {formState.errors.userName ? (
           <p>{formState.errors.userName.message}</p>
@@ -40,15 +40,15 @@ const Form = () => {
           email*
         </label>
         <input type="text" id="email"
-          {...register("email"),
-          {
-            required: true,
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message:
-                "El email no es válido, revisa la ortografía ❌",
-            },
-          }}
+          {...register("email",
+            {
+              required: true,
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message:
+                  "El email no es válido, revisa la ortografía ❌",
+              },
+            })}
         />
         {formState.errors.password ? (
           <p>{formState.errors.email.message}</p>
@@ -60,15 +60,15 @@ const Form = () => {
           contraseña*
         </label>
         <input type="text" id="password"
-          {...register("password"),
-          {
-            required: true,
-            pattern: {
-              value: /^[0-9]{8}[A-Za-z]$/,
-              message:
-                "La contraseña debe incluir números, letras Mayúsculas y minúsculas y como máximos 8 caracteres ❌",
-            },
-          }}
+          {...register("password",
+            {
+              required: true,
+              pattern: {
+                value: /^[0-9]{8}[A-Za-z]$/,
+                message:
+                  "La contraseña debe incluir números, letras Mayúsculas y minúsculas y como máximos 8 caracteres ❌",
+              },
+            })}
         />
         {formState.errors.password ? (
           <p>{formState.errors.userName.message}</p>
