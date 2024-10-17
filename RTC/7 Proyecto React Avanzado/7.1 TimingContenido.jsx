7b074f0e2f8228282705cbce8d19ea1916bedbf3
quintero git <hello.quinteroo@gmail.com>
2024-10-17 22:11:06 +0200

    ou yeessss

---
+++ RTC/7 Proyecto React Avanzado/7.1 TimingContenido.jsx
@@ -0,0 +1,17 @@
+
+
+
+//! TIMING CONTENIDO
+// 00.30 crea proyecto VITE
+// 01.00 - 06.00 revisa la API
+// 06.10 limpia proyecto
+// 06.40 instala librerías
+// router-dom, hook-form, chakra ui
+// 08.00 BrowserRouter y ChacraProvider en <App />
+
+//? =========== PROVIDER DARK/LIGHT ==========
+// 10.00 busca imagen sol y luna para el mode
+// 11.20 icono picachu para html
+//* 12.00 empieza a crear componente para luego extrarlo
+// 13.00 utiliza componente Image de Chakra
+//  
