5cba41e08b53d4425b0d0d9b431e8c4c0a90a55c
quintero git <hello.quinteroo@gmail.com>
2024-10-21 21:03:54 +0200

    chakra UI y styled components

---
+++ RTC/9 Chakra UI Styled Components/9.1 TimingContenido.jsx
@@ -0,0 +1,65 @@
+
+
+//! TIMING CONTENIDO
+// 00.00 crea proyecto chajra_ui y otro de styled components
+
+//? === Chakra UI ====
+// 02.45 explica librería y documentación
+// 03.48 instala librería
+// 04.27 coloca el Provider
+// 05.43 envuelve App con ChakraProvider
+// cambia el componente botón los estilos por defecto al añadir chakra al proyecto
+//* 08.00 explica porque con Chakra no se utiliza etiquetasde html puro
+// 08.30 13.00 - juega con colores de botones en la documentación
+// 13.01 explica como sería por dentro un compoenente de Chakra
+// 14.00 prueba diferetes props de compoenntes
+// 19.00 mete iconos en botones
+//* 22.00 loading en botón
+//* 26.00 FLEX
+// 34.00 utiliza CSS con Chakra
+// 39.00 componente FLEX
+// 40.15 componente GRID
+// 41.00 checkbox
+// 42.00 - 49.00 repasa todos los componenetes un poco por encima
+// 49.00 Alerts
+//* 52.00 sección componentes en Doc
+// 57.00 Hooks de Chakra_UI
+// 01.00.00 Toast y alertas (notificaciones)
+
+
+
+//? ==== STYLED COMPONENTS =====
+// 01.08.00 limpia proyecto VITE
+// 01.09.00 instala librería
+// explica como se empieza a trabajar con librería
+// 01.09.30 - 01.14.00 crea un componente
+// 01.20.00 - usar JS en CSS
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
