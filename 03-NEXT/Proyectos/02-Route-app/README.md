2fa0ac685c63b283344b8be91a6775b05eb99734
quintero git <hello.quinteroo@gmail.com>
2025-02-07 16:54:24 +0100

    FIRST COMMIT 02-Route-app

---
+++ 03-NEXT/Proyectos/02-Route-app/README.md
@@ -0,0 +1,15 @@
+
+
+## Temas a abordar
+1. Creación de rutas.
+
+2. Layouts y templates.
+
+3. Metadata.
+
+4. El componente Link.
+
+5. Navegación imperativa.
+
+6. Redirecciones.
+
---
+++ 03-NEXT/Proyectos/02-Route-app/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <h1>hola mundo</h1>
+  );
+}
+
+//! page
+// un archivo page.tsx representa una interfaz de usuario en una ruta específica
+
+//! app/page.tsx
+// como app es el directorio princial page es lo primero que se representa
